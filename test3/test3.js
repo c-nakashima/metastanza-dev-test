64187b2a9fc5ff743544dbe6443bff8a3e36de75
c-nakashima <nakashima@penqe.com>
2021-01-03 00:54:41 +0900

    added thead

--- test3/test3.js
+++ test3/test3.js
@@ -55,63 +55,8 @@
                     current_page == numPages() ? nextButton.classList.add('opacity') : nextButton.classList.remove('opacity');
                 }
 
-                // let changePage = function (page) {
-                //     const listingTable = document.getElementById('listingTable');
-
-                //     if (page < 1) {
-                //         page = 1;
-                //     }
-                //     if (page > (numPages() - 1)) {
-                //         page = numPages();
-                //     }
-
-                //     // listingTable.innerHTML = "";
-
-                //     //tbodyのIDを取得(この中で処理します)
-                //     var tbody = document.getElementById('tbodyID');
-
-                //     for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
-                //         //tr エレメントを新規作成(ただ生成するだけ)
-                //         var tr = document.createElement('tr');
-                //         //列(td)用のループ
-                //         for (j = 0; j < 5; j++){
-                //             //tdエレメントをを生成
-                //             var td = document.createElement('td');
-                //             //tdの中に入れたいモノをセット
-                //             td.innerHTML = 'こんにちは' + j;
-                //             //生成したtdをtrにセット
-                //             tr.appendChild(td);
-                //         }//列用のループ閉じ
-                //         //tr エレメントをtbody内に追加(ここではじめて表示される)
-                //         tbody.appendChild(tr);
-                //     }
-
-                    // var rows = [];
-                    // var table = document.createElement("table");
-
-                    // // 表に2次元配列の要素を格納
-                    // for(i = 0; i < objJson.length; i++){
-                    //     rows.push(table.insertRow(-1));  // 行の追加
-                    //     for(var j = 0; j < objJson[0].length; j++){
-                    //         cell=rows[i].insertCell(-1);
-                    //         cell.appendChild(document.createTextNode(objJson[i][j].value));
-                    //         // 背景色の設定
-                    //         if(i==0){
-                    //             cell.style.backgroundColor = "#bbb"; // ヘッダ行
-                    //         }else{
-                    //             cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
-                    //         }
-                    //     }
-                    // }
-                    // // 指定したdiv要素に表を加える
-                    // document.getElementById("listingTable").appendChild(table);
-
-                    // checkButtonOpacity();
-                    // selectedPage();
-                // }
                 let changePage = function (page) {
-                    const listingTable = document.getElementById('listingTable');
-
+                    
                     if (page < 1) {
                         page = 1;
                     }
@@ -119,29 +64,35 @@
                         page = numPages();
                     }
 
-                    // listingTable.innerHTML = "";
-                    
-                    listingTable.innerHTML += "<tbody id='tbodyID'></tbody>";
-                    const tbody = document.getElementById('tbodyID');
+                    var tableKey = Object.keys(objJson[0]); //th・tdに格納されるデータのキーを配列で取得してtableKeyに代入
+                    var columns_per_row = tableKey.length; //tableKeyで取得したキーの配列の数（＝列の数）を取得してcolumns_per_rowに代入
 
+                    //▼theadの描画
+                    const thead = document.getElementById('theadID');
+                    thead.innerHTML = "";
+                    thead.innerHTML = "<tr id='theadRowID'></tr>";
+
+                    for(var j = 0; j< columns_per_row; j++){
+                        var th = document.createElement("th");
+                        th.innerHTML = tableKey[j];
+                        var tr = document.getElementById('theadRowID');
+                        tr.appendChild(th);            
+                    }
+                    
+                    //▼tbodyの描画
+                    const tbody = document.getElementById('tbodyID');
                     tbody.innerHTML = "";
 
                     for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
                         var tr = document.createElement("tr");
-                        for(var j = 0; j< 5; j++){
+                        //それぞれのtrごとにtdを追加し、そのtdに対応する列のデータを格納する
+                        //ゆえに、ループする数はcolumns_per_row（＝列の数）回（ループ数=tdの数=列の数）
+                        for(var j = 0; j< columns_per_row; j++){
                             var td = document.createElement("td");
-                            // console.log(Object.keys(objJson[i]));
-                            // var tdKey = Object.keys(objJson[i]);
                             var tdValue = Object.values(objJson[i]);
                             td.innerHTML = tdValue[j].value;
-                            // console.log(tdKey[j]);
-                            console.log(tdValue[j].value);
                             tr.appendChild(td);
                         }
-                        
-                        // tbody.innerHTML += "<tr class=><tr>" ;
-                        // const 
-                        // listingTable.innerHTML += "<div class='objectBlock'>" + objJson[i].id.value + "</div>";
                         tbody.appendChild(tr);
                     }
                     checkButtonOpacity();
