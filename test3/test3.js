9f74ce4f9c3631bdbb7c43bce2a626da141349f5
c-nakashima <nakashima@penqe.com>
2021-01-04 17:11:41 +0900

    ajusted order and data to display

--- test3/test3.js
+++ test3/test3.js
@@ -12,9 +12,9 @@
         .then(data => {
             // responce.json()で受け取った中身を確認
             // 実際にはここでdataの中身を利用して様々な処理をする
-            // console.log(data.body);
-            let objJson = data.body;
-            console.log(objJson);
+            let dataHead = data.head;
+            console.log(dataHead);
+            let dataBody = data.body;
 
 
             function Pagination() {
@@ -22,27 +22,44 @@
                 const nextButton = document.getElementById('button_next');
                 const firstButton = document.getElementById('button_first');
                 const lastButton = document.getElementById('button_last');
+                // const sortButton = document.getElementById('button_sort');
                 const clickPageNumber = document.querySelectorAll('.clickPageNumber');
 
                 let current_page = 1;
                 let records_per_page = 5;
-                let total_records = objJson.length;
+                let total_records = dataBody.length;
+                
+                //orderを定義（負の値があったときは排除して新たに定義し、それ以外の場合はカラムの数だけorderを定義する）
+                let order = [];
+                if(dataHead.order){
+                    for(let i = 0; i < dataHead.order.length; i++){
+                        if(parseInt(dataHead.order[i]) >= 0){
+                            order[parseInt(dataHead.order[i])] = i;
+                        }
+                    }
+                }else{
+                    order = [...Array(dataHead.vars.length).keys()];
+                }
 
+                let tableKey = Object.keys(dataBody[0]); //th・tdに格納されるデータのキーを配列で取得してtableKeyに代入
+                let columns_per_row = tableKey.length; //tableKeyで取得したキーの配列の数（＝列の数）を取得してcolumns_per_rowに代入
+                
                 this.init = function () {
                     changePage(1);
                     pageNumbers();
                     selectedPage();
                     clickPage();
                     addEventListeners();
-                    // showingRecordsNumber(current_page);
                 }
-
+                
                 let addEventListeners = function () {
                     prevButton.addEventListener('click', prevPage);
                     nextButton.addEventListener('click', nextPage);
                     firstButton.addEventListener('click', firstPage);
                     lastButton.addEventListener('click', lastPage);
+                    // sortButton.addEventListener('click', sortColumn);
                 }
+                
 
                 let selectedPage = function () {
                     let page_number = document.getElementById('page_number').getElementsByClassName('clickPageNumber');
@@ -62,13 +79,6 @@
                 }
 
                 let showingRecordsNumber = function (page){
-                    // if (page < 1) {
-                    //     page = 1;
-                    // }
-                    // if (page > (numPages() - 1)) {
-                    //     page = numPages();
-                    // }
-
                     const showing_records_number = document.getElementById("showing_records_number");
 
                     let firstrecord_per_page = ((page - 1) * records_per_page) + 1;
@@ -82,47 +92,61 @@
                     showing_records_number.innerHTML = "<p>Showing" + firstrecord_per_page + "to" + lastrecord_per_page + "of" + total_records + "entres</p>";
                 }
 
-                let changePage = function (page) {
-                    
-                    // if (page < 1) {
-                    //     page = 1;
-                    // }
-                    // if (page > (numPages() - 1)) {
-                    //     page = numPages();
-                    // }
-
-                    var tableKey = Object.keys(objJson[0]); //th・tdに格納されるデータのキーを配列で取得してtableKeyに代入
-                    var columns_per_row = tableKey.length; //tableKeyで取得したキーの配列の数（＝列の数）を取得してcolumns_per_rowに代入
+                let changePage = function (page) {                    
+                    if (page < 1) {
+                        page = 1;
+                    }
+                    if (page > (numPages() - 1)) {
+                        page = numPages();
+                    }
 
                     //▼theadの描画
                     const thead = document.getElementById('theadID');
                     thead.innerHTML = "";
                     thead.innerHTML = "<tr id='theadRowID'></tr>";
-                        //▽trおよびthの描画
-                    for(var j = 0; j< columns_per_row; j++){
-                        var th = document.createElement("th");
-                        th.innerHTML = tableKey[j] + "<span class='icon filtericon'></span><span class='icon sorticon'></span>";
-                        var tr = document.getElementById('theadRowID');
-                        tr.appendChild(th);            
+                    //▽trおよびthの描画
+                    for(let i of order){
+                        let tr = document.getElementById('theadRowID');
+                        let th = document.createElement("th");
+                        let span_filter = document.createElement("span");
+                        let span_sort = document.createElement("span");
+                        let label = dataHead.vars[i];
+                        if(dataHead.labels){
+                            label = dataHead.labels[i];
+                        }
+                        th.innerHTML = label;
+                        span_filter.classList.add("icon", "filtericon");
+                        span_sort.setAttribute("data-type", dataHead.vars[i]);
+                        span_sort.classList.add("icon", "sorticon");
+
+                        th.appendChild(span_filter);
+                        th.appendChild(span_sort);
+                        tr.appendChild(th);
                     }
-                    
+
                     //▼tbodyの描画
                     const tbody = document.getElementById('tbodyID');
                     tbody.innerHTML = "";
-
-                    //▽trおよびtdの描画
-                    for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
-                        var tr = document.createElement("tr");
-                        //それぞれのtrごとにtdを追加し、そのtdに対応する列のデータを格納する
-                        //ゆえに、ループする数はcolumns_per_row（＝列の数）回（ループ数=tdの数=列の数）
-                        for(var j = 0; j< columns_per_row; j++){
-                            var td = document.createElement("td");
-                            var tdValue = Object.values(objJson[i]);
-                            td.innerHTML = tdValue[j].value;
+                    
+                    // //▽trおよびtdの描画
+                    for(var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < dataBody.length; i++){
+                        let tr = document.createElement("tr");
+                        for(let j of order){
+                            let td = document.createElement("td");
+                            // let tdValue = Object.values(dataBody[i]);
+                            if (dataHead.href[j]) {
+                                let a = document.createElement("a");
+                                a.setAttribute("href", dataBody[i][dataHead.href[j]].value);
+                                a.innerHTML = dataBody[i][dataHead.vars[j]].value;
+                                td.appendChild(a);
+                            } else {
+                                td.innerHTML = dataBody[i][dataHead.vars[j]].value;
+                            }
+                            tbody.appendChild(tr);
                             tr.appendChild(td);
                         }
-                        tbody.appendChild(tr);
                     }
+
                     checkButtonOpacity();
                     selectedPage();
                     showingRecordsNumber(current_page);
@@ -134,6 +158,21 @@
                         changePage(current_page);
                     }
                 }
+
+                // let sortColumn = function (e){
+                //     let span_sort = document.getElementsByClassName("sorticon");
+                //     for(var i = 0; i< columns_per_row; i++){
+                //         span_sort.setAttribute("data-type", tableKey[i]);
+                //         let offsetY = e.offsetY; // =>要素左上からのy座標
+                //         if(offsetY >= 8){
+                //             span_sort.className = "icon sorticon-asc";
+                //             const key = e.path[0].getAttribute('data-type');
+                //             const sortArray = dataBody.sort((a,b) => a[key].value.toLowerCase() < b[key].value.toLowerCase() ? -1 : 1);
+                //             console.log(sortArray);
+                //             changePage(current_page);
+                //         }
+                //     }
+                // }
 
                 let nextPage = function () {
                     if (current_page < numPages()) {
@@ -175,7 +214,7 @@
                 }
 
                 let numPages = function () {
-                    return Math.ceil(objJson.length / records_per_page);
+                    return Math.ceil(dataBody.length / records_per_page);
                 }
             }
             let pagination = new Pagination();
